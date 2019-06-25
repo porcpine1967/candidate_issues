9bb14190a6854fc21c71f067706839157f87379b
Jeffrey Dettmann <jeffrey.dettmann@gmail.com>
2019-06-25 18:04:11 -0400

    Biden

--- parse_candidates.py
+++ parse_candidates.py
@@ -2,27 +2,23 @@
 
 import re,  datetime, HTMLParser
 
-BLOCK_TAGS = ['h2', 'h3', 'p', 'div', 'article',]
-INLINE_TAGS = ['span', 'a', 'i', 'b', 'strong',]
+BLOCK_TAGS = ['h1', 'h2', 'h3', 'p', 'div', 'article', 'header', 'section', 'li', 'blockquote']
+INLINE_TAGS = ['span', 'a', 'i', 'b', 'strong', 'figure', 'img', 'ul']
 
 class ContentHTMLParser(HTMLParser.HTMLParser):
-    def __init__(self, html_file, candidate):
+    def __init__(self, html_file, content_tag, lines):
         HTMLParser.HTMLParser.__init__(self)
-        self.tag = candidate.content_tag
+        self.tag = content_tag
         self.file_as_string = ""
         for l in html_file:
             self.file_as_string += l
         self.tags = []
-        self.lines = []
+        self.lines = lines
 
     def handle_starttag(self, tag, attrs):
         if len(self.tags) or tag == self.tag:
             self.tags.append(tag)
-            if tag in BLOCK_TAGS:
-                self.lines.append('')
-            elif tag not in INLINE_TAGS:
-                raise StandardError(tag)
-
+            self.lines.append('')
     def handle_endtag(self, tag):
         if not self.tags:
             return
@@ -30,6 +26,10 @@ class ContentHTMLParser(HTMLParser.HTMLParser):
             self.tags = []
             return
         self.tags.pop()
+        if tag in BLOCK_TAGS:
+            self.lines.append('')
+        elif tag not in INLINE_TAGS:
+            raise StandardError(tag)
 
     def handle_data(self, data):
         if self.tags:
@@ -37,14 +37,14 @@ class ContentHTMLParser(HTMLParser.HTMLParser):
                 
 
 class NavigationHTMLParser(HTMLParser.HTMLParser):
-    def __init__(self, html_file, candidate):
+    def __init__(self, html_file, navigation_tag, links):
         HTMLParser.HTMLParser.__init__(self)
-        self.tag = candidate.navigation_tag
+        self.tag = navigation_tag
         self.file_as_string = ""
         for l in html_file:
             self.file_as_string += l
         self.tags = []
-        self.links = set()
+        self.links = links
 
     def handle_starttag(self, tag, attrs):
         if len(self.tags) or tag == self.tag:
@@ -72,20 +72,24 @@ class Candidate(object):
         self.name = name
         self.navigation_tag = navigation_tag
         self.content_tag = content_tag
+        self.links = set()
+        self.lines = set()
 
 def test_navigation():
-    c = Candidate('bennet', 'nav', '')
-    cp = NavigationHTMLParser(open('bennet.html'), c)
+    c = Candidate('biden', 'nav', '')
+    cp = NavigationHTMLParser(open('biden.html'), c.navigation_tag, c.links)
     cp.feed(cp.file_as_string)
-    for link in sorted(list(cp.links)):
+    for link in sorted(list(c.links)):
         print link
 
 def test_content():
-    c = Candidate('bennet', 'nav', 'article')
-    cp = ContentHTMLParser(open('bennet.html'), c)
+    c = Candidate('biden', 'nav', 'article')
+    c_lines = []
+    cp = ContentHTMLParser(open('biden.html'), c.content_tag, c_lines)
     cp.feed(cp.file_as_string)
-    lines = [l.strip() for l in cp.lines]
+    lines = [l.strip() for l in c_lines]
     for line in [l for l in lines if l]:
         print line
 if __name__ == '__main__':
+#    test_navigation()
     test_content()
