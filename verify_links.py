9cd6bf21f5206aad5a9475b53a38a86af20fe11e
porcpine1967 <jeffrey.dettmann@gmail.com>
2020-01-25 11:52:29 -0500

    update verification, add old pages

--- verify_links.py
+++ verify_links.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
 """
-Make sure all links in candidates.py are in data/changes/**/links
+Make sure all links in candidates.py are in data/changes/**/links and vice versa
 """
 from candidates import CANDIDATES
+
+KEEPERS = set()
+SKIPPERS = set()
+
+def load_keepers():
+    with open ('tokeep') as f:
+        for l in f:
+            url = l.strip()
+            KEEPERS.add(url)
+            if url.endswith('/'):
+                KEEPERS.add(url[0:-1])
+            else:
+                KEEPERS.add(url + '/')
+
+def load_skippers():
+    with open ('toskip') as f:
+        for l in f:
+            url = l.strip()
+            SKIPPERS.add(url)
+            if url.endswith('/'):
+                SKIPPERS.add(url[0:-1])
+            else:
+                SKIPPERS.add(url + '/')
 
 def validate_candidate(name, host, urls):
     links = set([host])
@@ -10,15 +33,21 @@ def validate_candidate(name, host, urls):
         for l in f:
             links.add(l.strip())
     for url in urls:
-        if url not in links:
-            print(name, url)
+        if url not in links and url not in KEEPERS:
+            print(url + ' not in ' + name + '/links')
+    for link in links:
+        if link.endswith('/'):
+            mod_link = link[0:-1]
+        else:
+            mod_link = link + '/'
+        if link not in urls and mod_link not in urls and link not in SKIPPERS:
+            print(link + ' not in candidates')
 
 if __name__ == '__main__':
-    candidate_links = {}
+    load_keepers()
+    load_skippers()
     for name, host, link_bundles in CANDIDATES:
-        candidate_urls = set()
-        candidate_links[name] = candidate_urls
+        candidate_urls = set((host,))
         for tag, attr, bad, urls in link_bundles:
-            for url in urls:
-                candidate_urls.add(url)
-        validate_candidate(name, host, urls)
+            candidate_urls.update(urls)
+        validate_candidate(name, host, candidate_urls)
